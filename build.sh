dba00e0474fe225324d0de0b9433ede79199913e
Joern Ott <joern.ott@ott-consult.de>
2019-03-03 21:39:03 +0000

    Bump versions

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@ curl -sSo src/tmp/install/functions.sh https://raw.githubusercontent.com/joernot
 source src/tmp/install/functions.sh
 
 patch_dockerfile DockerfileLTS
-docker build -f DockerfileLTS -t registry.ott-consult.de/oc/tiki:15.6 .
-docker tag registry.ott-consult.de/oc/tiki:15.6 registry.ott-consult.de/oc/tiki:lts
-docker push registry.ott-consult.de/oc/tiki:15.6
+docker build -f DockerfileLTS -t registry.ott-consult.de/oc/tiki:18.3 .
+docker tag registry.ott-consult.de/oc/tiki:18.3 registry.ott-consult.de/oc/tiki:lts
+docker push registry.ott-consult.de/oc/tiki:18.3
 docker push registry.ott-consult.de/oc/tiki:lts
 
-patch_dockerfile Dockerfile18
-docker build -f Dockerfile18 -t registry.ott-consult.de/oc/tiki:18.1 .
-docker tag registry.ott-consult.de/oc/tiki:18.1 registry.ott-consult.de/oc/tiki:latest
-docker push registry.ott-consult.de/oc/tiki:18.1
+patch_dockerfile Dockerfile19
+docker build -f Dockerfile19 -t registry.ott-consult.de/oc/tiki:19.1 .
+docker tag registry.ott-consult.de/oc/tiki:19.1 registry.ott-consult.de/oc/tiki:latest
+docker push registry.ott-consult.de/oc/tiki:19.1
 docker push registry.ott-consult.de/oc/tiki:latest
